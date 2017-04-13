25523f93d0763555ff2a76f216c846cff06159e2
Bruno Roy <brusi_roy@hotmail.com>
2017-04-12 23:14:52 -0400

    Add a class to do the parsing

--- trigraphs.cpp
+++ trigraphs.cpp
@@ -3,6 +3,45 @@
 #include <sstream>
 #include <string>
 
+enum class STATE { NORMAL, COMMENT, LITERAL };
+
+class Parser {
+   public:
+    Parser(const std::string& inputFileName, const std::string& outputFileName);
+    ~Parser();
+
+    void parse() noexcept;
+
+   private:
+    /// Buffer
+    std::stringstream buffer_;
+    /// Current state
+    STATE currentState_ = STATE::NORMAL;
+    /// Input file
+    std::ifstream inputFile_;
+    /// Ouput file
+    std::ofstream outputFile_;
+};
+
+Parser::Parser(const std::string& inputFileName,
+               const std::string& outputFileName)
+    : inputFile_(inputFileName), outputFile_(outputFileName) {
+    // TODO: Improve the reading speed
+    buffer_ << inputFile_.rdbuf();
+}
+
+Parser::~Parser() {
+    inputFile_.close();
+    outputFile_.close();
+}
+
+void Parser::parse() noexcept {
+    // Do the parsing
+
+    // Outputing
+    outputFile_ << buffer_.str();
+}
+
 /// Show the correct usage
 static void showUsage() {
     std::cout << "trigraphs: invalid arguments" << std::endl;
@@ -18,20 +57,8 @@ int main(int argc, char* argv[]) {
     const std::string inputFileName = argv[1];
     const std::string outputFileName = argv[2];
 
-    std::cout << inputFileName << std::endl;
-    std::cout << outputFileName << std::endl;
-
-    // Reading
-    std::ifstream inputFile(inputFileName.c_str());
-    // TODO: Improve reading speed
-    std::stringstream buffer;
-    buffer << inputFile.rdbuf();
-    inputFile.close();
-
-    // Writing
-    std::ofstream outputFile(outputFileName.c_str());
-    outputFile << buffer.str();
-    outputFile.close();
+    Parser parser(inputFileName, outputFileName);
+    parser.parse();
 
     return 0;
 }
